579e6957ea66643cf6b684b1a882c1031a087a38
David Rodriguez Miranda <wolfs_pride@outlook.es>
2021-05-06 21:07:18 +0200

    wip?

--- src/app.js
+++ src/app.js
@@ -1,34 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const userRouter = require('./routes/user')
+const port = process.env.PORT
+require('./db/db')
 
-var indexRouter = require('./routes/index');
+const app = express()
 
-var app = express();
+app.use(express.json())
+app.use(userRouter)
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/api', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port, () => {
+   console.log(`Server running on port ${port}`)
+})
